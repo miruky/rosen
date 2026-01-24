a785f904c9a56646b7d3bea37055ffbdf263a327
miruky <h.miyoki@gmail.com>
2026-01-24 13:46:02 +0900

    routeToTextを公開APIに追加

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -17,7 +17,7 @@ export type {
 export { matchStations, normalizeQuery, resolveStation } from './search';
 export { addRecent, parseRecent, RECENT_LIMIT } from './recent';
 export type { RecentEntry } from './recent';
-export { describeLeg, formatMinutes, summarizeRoute } from './format';
+export { describeLeg, formatMinutes, routeToText, summarizeRoute } from './format';
 export { escapeXml, mapViewBox, renderMap, renderRouteOverlay } from './render';
 export type { ViewBox } from './render';
 export { tokyoLines, tokyoStations, tokyoWalks } from './data';
